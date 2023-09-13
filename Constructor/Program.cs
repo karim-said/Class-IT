985d27acebb086a43f5db2d02ff4730af5f99935
Karim Said <inkriment@outlook.com>
2023-09-13 10:10:57 +0500

    Add project files.

---
+++ Constructor/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+namespace Constructor
+{
+    public class University
+    {
+        public int Num { get; set; }
+    }
+
+    public class A
+    {
+        private readonly University _ttu;     
+
+        public A(University ttu)
+        {
+            _ttu = ttu;
+        }
+
+
+        public A() { }
+        public A(string name) => Console.WriteLine(name);
+
+        
+        public int Id { get; set; }
+
+        //public void Foo()
+        //{
+        //    ttu.Num = 23;
+        //}
+
+        public void Chumchi()
+        {
+            Console.WriteLine("Chchi");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dmt = new University();
+
+            A a = new A(dmt);              
+        }
+    }
+}
---
+++ Property/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Property
+{
+        //private
+        //protected
+        //public
+    public class B
+    {
+        public string Move()
+        {
+            Console.WriteLine("Move from object B");
+            return "";
+        }
+    }
+
+    public class A
+    {
+        B _b = new B();
+
+        public A()
+        {
+            _b.Move();
+        }
+
+    }
+    internal class Program
+    {
+        static void Foo()
+        {
+            A a = new A();
+        }
+
+        static void Main(string[] args)
+        {
+            Foo();
+        }
+       
+    }
+}
