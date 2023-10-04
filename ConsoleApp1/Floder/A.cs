6126062e3e1ef561546aeab92a1e76cc7ba09ead
Karim Said <inkriment@outlook.com>
2023-10-04 10:34:08 +0500

    base constructor parameter pass

---
+++ ConsoleApp1/Floder/A.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1.Floder
+{
+ 
+    public class A 
+    {
+        public A()
+        {
+
+        }
+        public A(string a) => System.Console.WriteLine("Constructor from A" + a);
+    }
+
+    public class B : A
+    {
+        public B()
+        {
+
+        }
+        static string b = "Tojikiston ba pesh";
+        public B(int a) : base(b) => System.Console.WriteLine("Constructor from B");
+    }
+
+    public class C : B { }
+
+    public class Participant
+    {
+        public string EmployeeName { get; set; }
+        public int DocumentId { get; set; }
+        public int EmployeeId { get; set; }
+        public int OrganithationId { get; set; }
+    }
+
+    public class SenderDocument : Participant { }
+    public class RecipientDocument : Participant 
+    {
+        public bool Status { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Floder;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var b = new B(32);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
