5fd8d15aa912de031bb64a4fc2f46e77151fda5d
Karim Said <inkriment@outlook.com>
2023-10-11 20:54:30 +0500

    Abstract object

---
+++ Abstract/Abstract/Player.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace Abstract.Abstract
+{
+    public abstract class Weapon
+    {
+        public abstract int CountOfSome { get; }
+        public abstract void Shoot();
+    }
+
+    public class AK47 : Weapon
+    {
+        public override int CountOfSome => 30;
+        public override void Shoot()
+        {
+            System.Console.WriteLine("Qar Qar");
+        }
+    }
+
+    public class M16 : Weapon
+    {
+        public override int CountOfSome => 26;
+        public override void Shoot()
+        {
+            System.Console.WriteLine("Tuf Tuf");
+        }
+    }
+}
---
+++ Abstract/Program.cs
@@ -0,0 +1,17 @@
+﻿using Abstract.Abstract;
+using System;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AK47 a = new AK47(); 
+            a.Shoot();
+
+            M16 m = new M16(); 
+            m.Shoot();
+        }
+    }
+}
