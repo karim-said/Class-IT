5d31dae04b1a978657c2acae6faef2f7aa60dec2
Firdavs T <firdavs080902@gmail.com>
2023-10-13 09:57:28 +0500

    updated Task21 by Firdavs

--- Task21/main.go
+++ Task21/main.go
@@ -1,36 +1,58 @@
 package main
 
-import "projector-class/models"
+import (
+	"fmt"
+	"projector-class/models"
+)
 
 func main() {
 
-	size := models.Size{
-		Height: 1080,
-		Width:  1920,
-	}
-
-	focus := models.Focus{
-		FocusDistance: 100,
-	}
-
-	mode := models.Mode{
-		Name: "HDMI",
-	}
-
 	proj := &models.Projector{
-		Name:   "Canon",
-		Size:   size,
-		Focus:  focus,
-		Mode:   mode,
+		Name: "Canon",
+		Size: models.Size{
+			Height: 1920,
+			Width:  1080,
+		},
+		Focus: models.Focus{
+			FocusDistance: 10,
+		},
+		Mode: models.Mode{
+			Name: "HDMI",
+		},
 		Status: false,
 	}
 
 	var projectorIF models.ProjectorInterface
 	projectorIF = proj
 
-	projectorIF.SetCustomSize(720, 1280)
-	projectorIF.SetFocus(models.Focus{FocusDistance: 150})
-	projectorIF.SetMode(models.Mode{Name: "VGA"})
-	projectorIF.TurnOn()
-	projectorIF.GetInfo()
+	for {
+
+		fmt.Println("===================================================")
+
+		fmt.Printf("Введите число для настройки проектора.\n " +
+			"1 - Настроить размер\n " +
+			"2 - Настроить фокус\n " +
+			"3 - Настроить режим\n " +
+			"4 - Включить/Выключить\n " +
+			"5 - Информация о проекторе\n ")
+
+		var command int
+		fmt.Scan(&command)
+
+		switch command {
+		case 1:
+			projectorIF.SetCustomSize()
+		case 2:
+			projectorIF.SetFocus()
+		case 3:
+			projectorIF.SetMode()
+		case 4:
+			projectorIF.Turn()
+		case 5:
+			projectorIF.GetInfo()
+		default:
+			fmt.Println("Неизвестная команда")
+		}
+
+	}
 }
--- Task21/models/models.go
+++ Task21/models/models.go
@@ -3,11 +3,10 @@ package models
 import "fmt"
 
 type ProjectorInterface interface {
-	SetCustomSize(height, width int)
-	SetFocus(focus Focus)
-	SetMode(mode Mode)
-	TurnOff()
-	TurnOn()
+	SetCustomSize()
+	SetFocus()
+	SetMode()
+	Turn()
 	GetInfo()
 }
 
@@ -32,30 +31,58 @@ type Projector struct {
 	Status bool
 }
 
-func (p *Projector) SetCustomSize(height, width int) {
-	p.Size.Height = height
-	p.Size.Width = width
+func (p *Projector) SetCustomSize() {
+	var height, width int
+	fmt.Print("Введите  высоту: ")
+	fmt.Scan(&height)
+	fmt.Print("Введите  ширину: ")
+	fmt.Scan(&width)
+	size := Size{
+		Height: height,
+		Width:  width,
+	}
+	p.Size = size
 	fmt.Printf("Установлены пользовательские размеры проектора: %d на %d\n", height, width)
 }
 
-func (p *Projector) SetFocus(focus Focus) {
+func (p *Projector) SetFocus() {
+	var focusDistance int
+	fmt.Print("Введите  фокусное расстояние: ")
+	fmt.Scan(&focusDistance)
+	focus := Focus{
+		FocusDistance: focusDistance,
+	}
 	p.Focus = focus
 	fmt.Printf("Установлен фокусное расстояние %d мм\n", focus.FocusDistance)
 }
 
-func (p *Projector) SetMode(mode Mode) {
+func (p *Projector) SetMode() {
+	fmt.Printf("Выберите режим проектора.\n " +
+		"1 - HDMI\n " +
+		"2 - VGA\n")
+	var modeNumber int
+	fmt.Scan(&modeNumber)
+	var mode Mode
+	switch modeNumber {
+	case 1:
+		mode = Mode{Name: "HDMI"}
+	case 2:
+		mode = Mode{Name: "VGA"}
+	default:
+		fmt.Println("Неизвестная команда")
+	}
 	p.Mode = mode
 	fmt.Printf("Установлен режим %s\n", mode.Name)
 }
 
-func (p *Projector) TurnOff() {
-	p.Status = false
-	fmt.Println("Проектор выключен")
-}
-
-func (p *Projector) TurnOn() {
-	p.Status = true
-	fmt.Println("Проектор включен")
+func (p *Projector) Turn() {
+	if p.Status {
+		p.Status = false
+		fmt.Println("Проектор выключен")
+	} else {
+		p.Status = true
+		fmt.Println("Проектор включен")
+	}
 }
 
 func (p *Projector) GetInfo() {
