8f294429943e58b26fdfaac832c2f595e6866f11
n-aslik <139547543+n-aslik@users.noreply.github.com>
2023-10-21 20:47:04 +0500

    Add files via upload

---
+++ DrelLibrary/Drel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrelLibrary
+{
+    public abstract class Drel
+    {
+        public abstract string Name { get; }
+        public abstract bool Revers { get; }
+        public abstract int Volume { get; }
+        public bool Status { get; set; }
+
+
+
+        public abstract void Start();
+        public abstract void Stop();
+        public abstract void Move();
+        public abstract void Reverse();
+        public abstract void Speed();
+
+
+
+
+
+    }
+    public class Perfarator : Drel
+    {
+
+
+        public override string Name => "TOTAL";
+
+        public override int Volume => 0;
+
+        public override bool Revers => false;
+
+
+
+        public override void Start()
+        {
+
+            Task.Delay(100);
+            if (!Status)
+            {
+                Status = true;
+                System.Console.WriteLine($"Дрель {Name} включён");
+                Move();
+            }
+        }
+        public override void Stop()
+        {
+            if (Status)
+            {
+                Status = false;
+                System.Console.WriteLine($"Дрель {Name} выключен");
+            }
+        }
+        public override void Move()
+        {
+            Console.WriteLine("TRRRRRRRRRRRRRRRRRRRRRRRR");
+        }
+
+
+        public override void Reverse()
+        {
+
+            if (!Revers)
+            {
+                
+                Console.WriteLine($"Дрель {Name} крутиться назад");
+            }
+            else
+            {
+
+                Move();
+            }
+        }
+
+        public override void Speed()
+        {
+
+            if (Volume == 0)
+            {
+
+                System.Console.WriteLine($"Дрель {Name} поставлен на скорость:");
+
+
+            }
+
+            else
+            {
+                Stop();
+            }
+        }
+    }
+}
+    
+
---
+++ Drelclass/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrelLibrary;
+
+namespace Drelclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool turnstatus = true;
+            int score = 0;
+            int command = 0;
+            var Master = new Perfarator();
+
+            while(turnstatus)
+            {
+                Console.WriteLine("Команды Дреля"+
+                    "1-Start"+
+                    "2-Reverse"+
+                    "3-Speed"+
+                    "4-Stop");
+                command =Convert.ToInt32(Console.ReadLine());
+                switch(command)
+                {
+                    case 1:
+                        Master.Start();
+                        break;
+                    case 2:
+                        Master.Reverse();
+                        break;
+                    case 3:
+                        if(score==100)
+                        {
+                            Console.WriteLine($"Максимальная скорость: {score} ");
+                        }
+                        else
+                        {
+                            score = score + 5;
+                            Master.Speed();
+                            Console.WriteLine($"{Master.Volume + score}") ;
+                        }
+                        break;
+                    case 4:
+                        Master.Stop();
+                        break;
+                    default: 
+                        Console.WriteLine("Не распознанная команда!");
+                        break;
+                }
+            }
+
+
+        }
+    }
+}
