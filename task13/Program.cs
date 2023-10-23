e88b446decaefcadc72e85191e4d1a18597894d7
Rahimzoooda <sheralibahriddin@gmail.com>
2023-10-23 10:19:58 +0500

    Task13 - Rahimzoda

---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace tassk_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Mouse mouse = new Mouse(100, 1, 5000, 100000, true);
+            mouse.On();
+            mouse.Off();
+            mouse.Click();
+            mouse.MoveBack();
+            mouse.MoveFront();  
+            mouse.MoveLeft();
+            mouse.MoveRight();
+            mouse.DoubleClick();
+            mouse.EventKeyUp();
+            mouse.EventKeyDown();
+        }
+    }
+}
