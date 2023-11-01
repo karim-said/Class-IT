4c1d66c6025e8673ae7cf39dac8e544009672560
Karim Said <inkriment@outlook.com>
2023-11-01 09:31:24 +0500

    Pass struct & class  to a method

---
+++ Sturct/struct/point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoliNav
+{
+    public struct PointStruct
+    {
+        public int x, y;
+        
+        
+ 
+    }
+
+    public class PointClass
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+
+
+}
