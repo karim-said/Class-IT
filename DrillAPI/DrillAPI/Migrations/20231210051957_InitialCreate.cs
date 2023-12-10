383cf9b87676f79ac57b5f1900d8cf49e77efaf8
Nabiev Asliddin <aslnabiev2002@gmail.com>
2023-12-10 10:57:48 +0500

    add my api

---
+++ DrillAPI/DrillAPI/Migrations/20231210051957_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DrillAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Status = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Mode = table.Column<string>(type: "TEXT", nullable: true),
+                    Reverse = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Spped = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drills", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Drills");
+        }
+    }
+}
---
+++ DrillAPI/DrillAPI/models/Drill.cs
@@ -0,0 +1,12 @@
+﻿namespace DrillAPI.models
+{
+    public class Drill
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool Status { get; set; }
+        public string? Mode { get; set; }
+        public bool Reverse { get; set; }
+        public int Spped { get; set; }
+    }
+}
