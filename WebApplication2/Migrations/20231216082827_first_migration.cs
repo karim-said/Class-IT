7ad82075bb266f5b4b349fc5fa2680478ba6519d
Rahimzoooda <sheralibahriddin@gmail.com>
2023-12-16 13:53:30 +0500

    Ustod Man Rahimzoda In Projecti man Man tarzi dokhil kardani mushakro dar BD sqllite soxtam

---
+++ WebApplication2/Migrations/20231216082827_first_migration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mouse",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Company = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mouse", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mouse");
+        }
+    }
+}
---
+++ WebApplication2/model/Mouse.cs
@@ -0,0 +1,9 @@
+﻿namespace Mouse_Is
+{
+    public class Mouse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+}
