c59bbba73d7825984e404786351d1161728662ac
jane <jane.stories@gmail.com>
2023-12-16 12:03:30 +0500

    Soliev I

---
+++ SolievBrowserAPI/BrowserAPI/browser/BrowserMethod.cs
@@ -0,0 +1,38 @@
+﻿namespace Soliev
+{
+    public class BrowserMethod : IBrowserMethod
+    {
+        public string AddPageToHome()
+        {
+            return string.Format("Add page to Complete!");
+        }
+
+        public Browser Back(Browser browser)
+        {
+            browser.CoutOfPage = "Back page!";
+            return browser;
+        }
+
+        public Browser EditUrl(Browser browser)
+        {
+            browser.Url = "Change url complete!";
+            return browser;
+        }
+
+        public Browser Next(Browser browser)
+        {
+            browser.CurrentPage = "Next page !";
+            return browser; 
+        }
+
+        public string Refresh()
+        {
+            return "Refresh complete!";
+        }
+
+        public string ShowHistory()
+        {
+            return "Show history!";
+        }
+    }
+}
