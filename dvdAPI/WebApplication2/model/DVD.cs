f8a5a9c77a3f8777fd4aebe80c865bca32adc56e
Shokirov0101 <shokirov20@outlook.com>
2023-12-16 12:49:20 +0500

    Shokirov

---
+++ dvdAPI/WebApplication2/model/DVD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace DVDAPI.model
+{
+    public class DVD
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool Status { get; set; }
+        public string? OutputMode { get; set; }
+        public string? DiskType { get; set; }
+        public int Volume { get; set; }
+        public bool Pause { get; set; }
+        public bool Stop { get; set; }
+        public bool Play { get; set; }
+        public bool Eject { get; set; }
+        public bool Close { get; set; }
+
+    }
+}
+
